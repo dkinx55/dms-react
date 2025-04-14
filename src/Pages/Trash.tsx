92fb0bfa9eb814f8d83fa44fc01a618f311dd7c1
dkinx55 <droidtechnologys@gmail.com>
2025-04-14 13:00:10 +0400

    DMS Basic

---
+++ src/Pages/Trash.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TrashPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-4">Trash</h1>
+      <p>Deleted documents go here.</p>
+    </div>
+  );
+};
+
+export default TrashPage;
